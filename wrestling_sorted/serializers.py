816bc0b2b876f5f9b73320dc2455374f637b1397
Reval Govender <revalgovender>
2023-12-21 21:30:38 +0000

    create endpoint to retrieve highlights

---
+++ wrestling_sorted/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Highlight
+
+
+class HighlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Highlight
+        fields = '__all__'
--- wrestling_sorted/views.py
+++ wrestling_sorted/views.py
@@ -1,21 +1,39 @@
-from rest_framework import status
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
-from wrestling_sorted.services.highlights import Highlights
+from wrestling_sorted.models import Highlight, Episode
+from wrestling_sorted.serializers import HighlightSerializer
 
 
-@api_view(["GET"])
-def list_tv_shows_highlights(request):
+@api_view(['GET'])
+def highlights(request, tv_show_id, episode_date):
     """
-    List the tv show's highlights.
+    List all highlights for given episode of a TV show.
     """
-    tv_shows_highlights = Highlights()
-    highlights = tv_shows_highlights.retrieve()
+    if request.method == 'GET':
+        # Get episode
+        episode = Episode.objects.get(tv_show_id=tv_show_id, episode_date=episode_date)
 
-    response = {
-        "status": 'success',
-        "message": 'Highlights retrieved successfully',
-        "data": highlights
-    }
-    return Response(response, status=status.HTTP_200_OK)
+        # Get highlights
+        highlights = Highlight.objects.filter(tv_show_id=tv_show_id, episode=episode)
+
+        # Paginate highlights
+        page_number = request.GET.get("page", 1)
+        paginator = Paginator(highlights, per_page=15)
+        page_object = paginator.get_page(page_number)
+        serializer = HighlightSerializer(page_object, many=True)
+
+        # Construct payload
+        payload = {
+            "page": {
+                "current": page_object.number,
+                "total": paginator.num_pages,
+            },
+            "highlights": {
+                "total": paginator.count,
+                "results": serializer.data,
+            }
+        }
+
+        return JsonResponse(payload, safe=False)
