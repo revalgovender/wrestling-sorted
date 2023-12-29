ae14d8e79ff9409d17f6ec9b8c75679e391f40c7
Reval Govender <revalgovender>
2023-12-29 19:01:13 +0000

    add error handling

--- app/wrestling_sorted/views.py
+++ app/wrestling_sorted/views.py
@@ -10,18 +10,36 @@ def highlights(request, tv_show_id, episode_date):
     """
     List all highlights for given episode of a TV show.
     """
+    # Initialize data.
+    payload = {
+        "status": "success",
+    }
+    tv_show_id = int(tv_show_id)
+
+    # Retrieve highlights.
     if request.method == 'GET':
-        episode = Episode.objects.get(tv_show_id=tv_show_id, episode_date=episode_date)
-        tv_show = episode.tv_show
-        highlights = Highlight.objects.filter(tv_show_id=tv_show_id, episode=episode)
-        serializer = HighlightSerializer(highlights, many=True)
+        try:
+            episode = Episode.objects.get(tv_show_id=tv_show_id, episode_date=episode_date)
+            tv_show = episode.tv_show
+            highlights = Highlight.objects.filter(tv_show_id=tv_show_id, episode=episode)
+            serializer = HighlightSerializer(highlights, many=True)
+            payload['data'] = {
+                "tv_show": tv_show.name,
+                "episode_date": episode_date,
+                "total_highlights": serializer.data.__len__(),
+                "highlights": serializer.data,
+            }
+        except Episode.DoesNotExist:
+            payload['status'] = "error"
+            payload['message'] = "Episode does not found."
+            return JsonResponse(payload, safe=False, status=404)
+        except Highlight.DoesNotExist:
+            payload['status'] = "error"
+            payload['message'] = "Highlight does not found."
+            return JsonResponse(payload, safe=False, status=404)
+        except Exception as e:
+            payload['status'] = "error"
+            payload['message'] = "Something went wrong."
+            return JsonResponse(payload, safe=False, status=500)
 
-        # Construct payload
-        payload = {
-            "tv_show": tv_show.name,
-            "episode_date": episode_date,
-            "total_highlights": serializer.data.__len__(),
-            "highlights": serializer.data,
-        }
-
-        return JsonResponse(payload, safe=False)
+    return JsonResponse(payload, safe=False)
