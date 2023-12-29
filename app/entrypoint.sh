ad6af48cc9a613b8e474e63b5dd180898c0b47e3
Reval Govender <revalgovender>
2023-12-29 19:23:21 +0000

    prevent migrations running on running imports

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-python manage.py wait_for_db
+# Function to run Django development server or Gunicorn
+run_server() {
+    if [ "$DJANGO_ENV" = "development" ]; then
+        python manage.py runserver 0.0.0.0:8000
+    else
+        gunicorn wrestling_sorted.wsgi:application --timeout=3000 --bind=0.0.0.0:8000
+    fi
+}
 
-python manage.py migrate
-
-python manage.py loaddata initial_data.json
-
-if [ "$DJANGO_ENV" = "development" ]; then
-    python manage.py runserver 0.0.0.0:8000
+# Check if any arguments are passed
+if [ $# -gt 0 ]; then
+    # If arguments are passed, execute them
+    exec "$@"
 else
-    gunicorn wrestling_sorted.wsgi:application --timeout=3000 --bind=0.0.0.0:8000
+    # No arguments, proceed with initial setup and run server
+    python manage.py wait_for_db
+    python manage.py migrate
+    python manage.py loaddata initial_data.json
+
+    run_server
 fi
